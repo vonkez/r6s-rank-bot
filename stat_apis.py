b231ce52f3a2bc8c5fbc64829508453b25cc2a03
Vonkez <vonkez21@gmail.com>
2020-04-24 05:47:35 +0300

    improve api error message

--- stat_apis.py
+++ stat_apis.py
@@ -87,7 +87,7 @@ class R6Stats:
                                              avatar256=json_resp['avatar_url_256'],
                                              level=json_resp['progression']['level'])
                     return player
-                elif resp.status == 404 or resp == 500:
+                elif resp.status == 404 or resp.status == 500:
                     return None
                 else:
                     print(resp.request_info)
@@ -114,7 +114,7 @@ class R6Stats:
                                              rank_image=json_resp['seasons']['void_edge']['regions']['emea'][0]['rank_image'],
                                              rank_short=json_resp['seasons']['void_edge']['regions']['emea'][0]['rank_text'].split()[0])
                     return player
-                elif resp.status == 404 or resp == 500:
+                elif resp.status == 404 or resp.status == 500:
                     return None
                 else:
                     print(resp.request_info)
